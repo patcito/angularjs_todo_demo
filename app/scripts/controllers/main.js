d427878a4cf636f36ca477f79dc5fcdeb6562478
Patrick Aljord <patcito@gmail.com>
2012-10-29 11:28:49 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+todoApp.controller('MainCtrl', function($scope) {
+/*
+  $scope.remaining = function() {
+    return $scope.todos.reduce(function(count, todo) {
+      return todo.done ? count : count + 1
+    }, 0);
+  };
+ 
+  $scope.archive = function() {
+    $scope.todos = $scope.todos.filter(function(todo){
+      return !todo.done
+    })
+  };
+*/  
+});
