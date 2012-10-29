36e47ed7fd87c2278e1386389191fe44c4b3d014
Patrick Aljord <patcito@gmail.com>
2012-10-29 13:21:44 +0100

    make todo work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,19 @@
 'use strict';
 
 todoApp.controller('MainCtrl', function($scope) {
-/*
+  $scope.name = "Pat";
+
+  $scope.todos = [
+    {text: "introduce AngularJS", done: true},
+    {text: "build a todo app", done: false},
+  ]
+
+  $scope.addTodo = function(){
+    console.log($scope)
+    $scope.todos.push({text: $scope.newTodo, 
+                      done: false});
+    $scope.newTodo = '';
+  }
   $scope.remaining = function() {
     return $scope.todos.reduce(function(count, todo) {
       return todo.done ? count : count + 1
@@ -13,5 +25,4 @@ todoApp.controller('MainCtrl', function($scope) {
       return !todo.done
     })
   };
-*/  
 });
